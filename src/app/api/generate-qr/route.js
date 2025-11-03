37999e86cf31b96fc7eef75d3d08749987f770b6
Prashant Kala <prasahnt.golu12@gmail.com>
2025-11-03 15:23:47 +0530

    APi url fix -4

--- src/app/api/generate-qr/route.js
+++ src/app/api/generate-qr/route.js
@@ -3,7 +3,7 @@ import { headers } from 'next/headers';
 import axios from 'axios';
 
 // Your backend server
-const BACKEND_API_URL = process.env.NEXT_PUBLIC_API_URL;
+const BACKEND_API_URL = process.env.API_URL;
 
 export async function POST(request) {
   try {
